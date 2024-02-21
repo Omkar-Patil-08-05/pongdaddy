36b94e1851f1c0d6e16f734c04a32087de29834c
vortex73 <nsreekumar6@gmail.com>
2024-02-21 18:17:22 +0530

    aah

--- networks/client.cpp
+++ networks/client.cpp
@@ -1,26 +1,49 @@
 #include <cstdio>
+#include <openssl/bio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <iostream>
 #include <ostream>
 #include <sys/socket.h>
+#include <thread>
 #include <unistd.h>
 #include <netinet/in.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 #include <arpa/inet.h>
+#include <vector>
 
-#define PORT 8080
+#define WPORT 8080
+#define RPORT 8123
 #define PIPE "/tmp/pongdaddy"
+
+float buf[2];
+float buff[1];
+float y = 0;
+void send_to_server(int pfd,int cfd){
+    while (true) {
+        read(pfd, buf+1, sizeof(buf));
+        int msg = send(cfd, buf, sizeof(buf), 0);
+    }
+}
+
+
 int main (int argc, char *argv[]) {
-    int cfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (cfd < 0) {
+    int writer = socket(AF_INET, SOCK_STREAM, 0);
+    int reader = socket(AF_INET, SOCK_STREAM, 0);
+    if (writer < 0) {
         std::cout << "[ERROR] socket creation failed" << std::endl;
     }
-    sockaddr_in csoc;
-    csoc.sin_family = AF_INET;
-    csoc.sin_port = htons(PORT);
-    inet_pton(AF_INET, ("192.168.1.7"),&csoc.sin_addr.s_addr);
-    if (connect(cfd, (sockaddr*)&csoc, sizeof(csoc)) < 0){
+    sockaddr_in writesoc;
+    writesoc.sin_family = AF_INET;
+    writesoc.sin_port = htons(WPORT);
+    sockaddr_in readsoc;
+    readsoc = writesoc;
+    readsoc.sin_port = htons(RPORT);
+    inet_pton(AF_INET, ("192.168.1.7"),&writesoc.sin_addr.s_addr);
+    inet_pton(AF_INET, ("192.168.1.7"),&readsoc.sin_addr.s_addr);
+    if (connect(writer, (sockaddr*)&writesoc, sizeof(writesoc)) < 0 && connect(reader, (sockaddr*)&readsoc, sizeof(readsoc)) < 0){
         std::cerr << "[ERROR] Failed to connect to server" << std::endl;
         return 0;
     }
@@ -28,25 +51,32 @@ int main (int argc, char *argv[]) {
         std::cout << "[INFO] Client connected to server" << std::endl;
     }
     if (access(PIPE, F_OK)==-1) {
-       mkfifo(PIPE, 0666); 
+        mkfifo(PIPE, 0666); 
     }
     int pfd = open(PIPE, O_RDONLY | O_NONBLOCK);
     if (pfd<0) {
         std::cerr << "[ERROR] Error opening pipe" << std::endl;
         return -1;
     }
-    float buf[1];
-    float buff[1];
-    float y = 0;
-    while (1) {
-        read(pfd, buf, sizeof(buf));
-       std::cout << "[CLIENT] " << buf[0] << std::endl;
-        int msg = send(cfd, buf, sizeof(buf), 0);
-       // int recev = recv(cfd, buff, sizeof(buff), 0) ;
-       // std::cout << "[RECEIVED] " << buff << std::endl;
-        usleep(160000);
+    std::vector<std::thread> channel;
+    
+    channel.emplace_back(send_to_server,pfd,writer);
+    channel.back().detach();
+    float readbuf[2] = {-1,-1};
+    while (true) {
+        int msg = recv(reader, readbuf, sizeof(readbuf), 0);
+        std::cout << msg << std::endl;
+        if (msg==0) break;
     }
-    close(cfd);
+    // while (1) {
+    //   read(pfd, buf, sizeof(buf));
+    // std::cout << "[CLIENT] " << buf[0] << std::endl;
+    // int msg = send(cfd, buf, sizeof(buf), 0);
+    // int recev = recv(cfd, buff, sizeof(buff), 0) ;
+    //std::cout << "[RECEIVED] " << buff << std::endl;
+    //        usleep(160000);
+    // }
+    close(writer);
     close(pfd);
     return 0;
 }
--- networks/server.cpp
+++ networks/server.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <iostream>
 #include <arpa/inet.h>
 #include <iterator>
@@ -8,46 +9,71 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
+#define RPORT 8080
+#define WPORT1 8123
+#define WPORT2 8007
+float buf[2] = {0,0};
+float buf1[2] = {0,0};
+float buf2[2] = {0,0};
 std::vector<std::thread> proc;
+std::vector<std::thread> sendlist;
+void send_to_client(int sender){
+    while(true){
+        if (buf[0]==0) {
+            int msg = send(sender, buf2, sizeof(buf2), 0);
+         //   std::cout << "[P1] " << buf1[1] << std::endl;
+        }
+        else{
+            std::cout << "[P2] " << buf2[1] << std::endl;
+        }
+
+      //  int msg = send(sender, buf, sizeof(buf), 0);
+    }
+}
 void process(int con){
-    float buf[1];
     while (true) {
         int msg = recv(con, buf, sizeof(buf), 0);
         if(msg == 0) break;
-        std::cout << "[SERVER] " << buf[0] << std::endl;
-//        for (std::thread& client : proc) {
-//            if (client.native_handle() != con) {
-//                send(client.native_handle(), buf, sizeof(buf), 0);
-//            }
-//        }
+        if (buf[0]==0) {
+            std::memcpy(buf1,buf,sizeof(buf));
+        }
+        else{
+            std::memcpy(buf2, buf, sizeof(buf));
+        }
+        //   std::cout << "[SERVER] " << buf[0] << std::endl;
     }
 }
-#define PORT 8080
 int main(){
-    int sfd = socket(AF_INET, SOCK_STREAM, 0);
-    sockaddr_in dat;
-    dat.sin_family = AF_INET;
-    dat.sin_port = htons(PORT);
-    dat.sin_addr.s_addr = INADDR_ANY;
+    int reader = socket(AF_INET, SOCK_STREAM, 0);
+    int writer = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in readsoc;
+    readsoc.sin_family = AF_INET;
+    readsoc.sin_port = htons(RPORT);
+    readsoc.sin_addr.s_addr = INADDR_ANY;
+    sockaddr_in writesoc;
+    writesoc = readsoc;
+    writesoc.sin_port = htons(WPORT1);
     //    inet_pton(AF_INET, ("127.0.0.1"), &dat.sin_addr.s_addr);
-    if (bind(sfd, (sockaddr*)&dat, sizeof(dat)) < 0){
+    if (bind(reader, (sockaddr*)&readsoc, sizeof(readsoc)) < 0 && bind(writer, (sockaddr*)&writesoc, sizeof(writesoc)) < 0){
         std::cerr << "[ERROR] Socket Bind Failed" << std::endl;
-        close(sfd);
+        close(reader);
         return 0;
     }
     else{
         std::cout << "[INFO] Socket bound" << std::endl;
     }
-    if (listen(sfd, 2) < 0){
+    if (listen(reader, 2) < 0 && listen(writer, 2) < 0){
         std::cerr << "[ERROR] Can't listen on socket" << std::endl;
-        close(sfd);
+        close(reader);
         return 0;
     }
     else{
         std::cout << "[INFO] Socket listening" << std::endl;
     }
     while (true) {
-        int con = accept(sfd, NULL, NULL);
+        int con = accept(reader, NULL, NULL);
+        int writecon = accept(writer, NULL, NULL);
+        // 2 threads for each client 
         if (con < 0){
             std::cerr << "[ERROR] Socket failed to accept connection" << std::endl;
             return -1;
@@ -55,10 +81,12 @@ int main(){
         else{
             std::cout << "[INFO] Socket Accepted connection from client" << std::endl;
         }
+        sendlist.emplace_back(send_to_client,writecon);
         proc.emplace_back(process,con);
+        sendlist.back().detach();
         proc.back().detach();
 
     }
-    close(sfd);
+    close(reader);
 
 }
