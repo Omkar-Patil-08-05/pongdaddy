3dc0af4324bea635c52084f4558688f3abd40807
vortex73 <nsreekumar6@gmail.com>
2024-03-06 16:21:01 +0530

    over

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ SSL_CTX* initialize_ssl_context(bool is_server) {
     // Load server certificate and private key
     if (is_server) {
         if (SSL_CTX_use_certificate_file(ctx, "/home/vorrtt3x/dev/pongdaddy/certificate.pem", SSL_FILETYPE_PEM) <= 0 ||
-            SSL_CTX_use_PrivateKey_file(ctx, "/home/vorrtt3x/dev/pongdaddy/key.pem", SSL_FILETYPE_PEM) <= 0) {
+                SSL_CTX_use_PrivateKey_file(ctx, "/home/vorrtt3x/dev/pongdaddy/key.pem", SSL_FILETYPE_PEM) <= 0) {
             ERR_print_errors_fp(stderr);
             exit(EXIT_FAILURE);
         }
@@ -107,26 +107,37 @@ class striker{
             }
         }
 };
-void send_to_peer(int socket_fd,std::string peer_ip, float* buffer) {
+void send_to_peer(int socket_fd,std::string peer_ip, float* buffer,SSL_CTX* ctx_sender) {
     sockaddr_in peer_addr;
     peer_addr.sin_family = AF_INET;
     inet_pton(AF_INET, peer_ip.c_str(), &peer_addr.sin_addr);
     peer_addr.sin_port = htons(RPORT_RECEIVE); // Port for receiving on the peer
-    int con = connect(socket_fd, (sockaddr*)&peer_addr, sizeof(peer_addr));
+    SSL* sender_ssl = SSL_new(ctx_sender);
+    int con = -1;
     while(con<0){
-        std::cout << "Failed to Connect" << std::endl;
+        std::cout << "Trying to Connect" << std::endl;
         sleep(5);
         con = connect(socket_fd, (sockaddr*)&peer_addr, sizeof(peer_addr));
+    }
+    if (SSL_set_fd(sender_ssl, socket_fd) == 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+    if (SSL_connect(sender_ssl) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
     }
 
     std::cout << "Connection Successful" << std::endl;
     while (true) {
-        send(socket_fd, buffer, sizeof(float) * 2, 0);
+        //        send(socket_fd, buffer, sizeof(float) * 2, 0);
+        SSL_write(sender_ssl, buffer, sizeof(float)*2);
         usleep(000); 
     }
+    
 }
 
-void receive_from_peer(int socket_fd, float* buffer) {
+void receive_from_peer(int socket_fd, float* buffer,SSL_CTX* ctx_receiver) {
     sockaddr_in sender_addr;
     socklen_t sender_len = sizeof(sender_addr);
     sender_addr.sin_family = AF_INET;
@@ -135,9 +146,22 @@ void receive_from_peer(int socket_fd, float* buffer) {
     int bin = bind(socket_fd, (sockaddr*)&sender_addr, sender_len);
     listen(socket_fd, 1);
     int client_fd = accept(socket_fd, (sockaddr*)&sender_addr, &sender_len);
-
+    SSL* receiver_ssl = SSL_new(ctx_receiver);
+    if (!receiver_ssl) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+    if (SSL_set_fd(receiver_ssl, client_fd) == 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+    if (SSL_accept(receiver_ssl) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
     while (true) {
-        recv(client_fd, buffer, sizeof(float) * 2, 0);
+//        recv(client_fd, buffer, sizeof(float) * 2, 0);
+        SSL_read(receiver_ssl, buffer, sizeof(float)*2);
         readbuf[1] = buffer[1];
         usleep(000); 
     }
@@ -154,11 +178,11 @@ int main () {
     std::cin >> peer_ip;
 
     // Sender connection
-    channel.emplace_back(send_to_peer, sender_socket, peer_ip ,buf);
+    channel.emplace_back(send_to_peer, sender_socket, peer_ip ,buf,ctx_sender);
     channel.back().detach();
 
     // Receiver connection
-    channel.emplace_back(receive_from_peer, receiver_socket, readbuf);
+    channel.emplace_back(receive_from_peer, receiver_socket, readbuf,ctx_receiver);
     channel.back().detach();
     std::cout << "[INFO] Starting Game" << std::endl;
     Puck ball;
